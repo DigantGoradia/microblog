f3bbd89c2ed122986064827d0f3b2a1b03c30187
Digant Goradia <d.goradia0112@gmail.com>
2022-05-20 15:03:36 -0400

    Added explore view

--- app/routes.py
+++ app/routes.py
@@ -17,10 +17,9 @@ def index():
         db.session.commit()
         flash('Your post is now live!')
         return redirect(url_for('index'))
-    return render_template('index.html', title='Home Page', form=form)
-
     posts = current_user.followed_posts().all()
     return render_template('index.html', title='Home Page', form=form, posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -135,3 +134,10 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
